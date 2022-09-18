c21317eec1252e2537c7da0c10a1415e1d6161ac
molodoysever <dpnofficial1@gmail.com>
2022-09-18 16:04:49 +0300

    lesson6 add hw

---
+++ src/ua/lyashko/lesson6/Task1/Main.java
@@ -0,0 +1,33 @@
+package ua.lyashko.lesson6.Task1;
+
+public class Main {
+    public static void main ( String[] args ) {
+        Phone phone1 = new Phone (  );
+        phone1.model = "iPhone";
+        phone1.number = 38095;
+        phone1.weight = 1;
+
+        Phone phone2 = new Phone (  );
+        phone2.model = "Samsung";
+        phone2.number = 38098;
+        phone2.weight = 2;
+
+        Phone phone3 = new Phone (  );
+        phone3.model = "Xiaomi 520 yader";
+        phone3.number = 38096;
+        phone3.weight = 3;
+
+        System.out.println ("model: " + phone1.model + " number: " + phone1.number + " weight: " + phone1.weight );
+        System.out.println ("model: " + phone2.model + " number: " + phone2.number + " weight: " + phone2.weight );
+        System.out.println ("model: " + phone3.model + " number: " + phone3.number + " weight: " + phone3.weight );
+
+        phone1.receiveCall ();
+        phone1.getNumber ();
+
+        phone2.receiveCall ();
+        phone2.getNumber ();
+
+        phone3.receiveCall ();
+        phone3.getNumber ();
+    }
+}
---
+++ src/ua/lyashko/lesson6/Task1/Phone.java
@@ -0,0 +1,30 @@
+package ua.lyashko.lesson6.Task1;
+
+public class Phone {
+    int number;
+    String model;
+    int weight;
+    String name = "Jack";
+
+    public Phone ( int number , String model , int weight ) {
+        this.number = number;
+        this.model = model;
+        this.weight = weight;
+    }
+
+    public Phone ( int number , String model ) {
+        this.number = number;
+        this.model = model;
+    }
+
+    public Phone () {
+    }
+
+    public void receiveCall() {
+        System.out.println ("Call: " + name );
+    }
+
+    public void getNumber() {
+        System.out.println (number);
+    }
+}
