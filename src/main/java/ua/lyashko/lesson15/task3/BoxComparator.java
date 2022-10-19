53a4d470c20598d47d91c02d396ddf99d9b24aab
molodoysever <dpnofficial1@gmail.com>
2022-10-19 15:30:43 +0300

    hw15 BoxComparator codestyle fixed

--- src/main/java/ua/lyashko/lesson15/task3/BoxComparator.java
+++ src/main/java/ua/lyashko/lesson15/task3/BoxComparator.java
@@ -4,11 +4,11 @@ import java.util.Comparator;
 
 public class BoxComparator implements Comparator<Box> {
     public int compare ( Box box1 , Box box2 ) {
-        if (box1.getVolume () == 0 && box2.getVolume () != 0) {
+        if (box1.getVolume ( ) == 0 && box2.getVolume ( ) != 0) {
             return -1;
-        } else if (box1.getVolume () == 0 && box2.getVolume () == 0) {
+        } else if (box1.getVolume ( ) == 0 && box2.getVolume ( ) == 0) {
             return 0;
-        } else if (box1.getVolume () != 0 && box2.getVolume () == 0 ) {
+        } else if (box1.getVolume ( ) != 0 && box2.getVolume ( ) == 0) {
             return 1;
         } else return box2.getVolume ( ).compareTo ( box1.getVolume ( ) );
     }
