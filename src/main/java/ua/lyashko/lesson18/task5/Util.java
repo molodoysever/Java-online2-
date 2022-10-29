a3c0b9147dcc0fe269320d75d11ebb9cfac87a72
molodoysever <dpnofficial1@gmail.com>
2022-10-29 11:30:42 +0300

    hw18 codestyle fixed

--- src/main/java/ua/lyashko/lesson18/task5/Util.java
+++ src/main/java/ua/lyashko/lesson18/task5/Util.java
@@ -5,25 +5,23 @@ import java.util.List;
 import java.util.Random;
 
 public class Util {
-    public static Boolean isSuitable (int minSizeLimit, int size ) {
+    public static Boolean isSuitable ( int minSizeLimit , int size ) {
         return minSizeLimit < size;
     }
 
-    public static List<Item> createItemList() {
-        List<Item> itemList = new ArrayList<> ();
-        Random random = new Random (  );
-        for (int i = 0; i < random.nextInt (10 ); i++) {
-            itemList.add ( new Item ( "abc", random.nextInt ( 100) ) );
+    public static List<Item> createItemList () {
+        List<Item> itemList = new ArrayList<> ( );
+        Random random = new Random ( );
+        for (int i = 0; i < random.nextInt ( 10 ); i++) {
+            itemList.add ( new Item ( "abc" , random.nextInt ( 100 ) ) );
         }
         return itemList;
     }
 
-    public static void fillBoxList ( List<Box> boxList) {
-        Random random = new Random (  );
+    public static void fillBoxList ( List<Box> boxList ) {
+        Random random = new Random ( );
         for (int i = 0; i < 10; i++) {
-            boxList.add ( new Box ( Util.createItemList (), random.nextInt ( 10) ) );
+            boxList.add ( new Box ( Util.createItemList ( ) , random.nextInt ( 10 ) ) );
         }
-
-
     }
 }
