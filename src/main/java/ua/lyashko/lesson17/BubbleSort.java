d34740aa388c7060d407139461f35f7b6796812f
molodoysever <dpnofficial1@gmail.com>
2022-10-30 21:17:21 +0200

    hw18 BubbleSort fixed

--- src/main/java/ua/lyashko/lesson17/BubbleSort.java
+++ src/main/java/ua/lyashko/lesson17/BubbleSort.java
@@ -8,13 +8,15 @@ public class BubbleSort {
         Comparator<Product> productComparator =
                 new CategoryComparator ( ).thenComparing ( new ProductComparator ( ) );
 
-        for (int i = 0; i < products.size ( ) - 1; i++) {
-            for (int j = 0; j < products.size ( ) - i - 1; j++) {
+        boolean unsorted = true;
+        while ( unsorted ) {
+            unsorted = false;
+            for (int i = 0; i < products.size ( ) - 1; i++) {
                 if (productComparator.compare ( products.get ( i ) , products.get ( i + 1 ) ) >= 0) {
                     Product temp = products.get ( i );
                     products.set ( i , products.get ( i + 1 ) );
                     products.set ( i + 1 , temp );
-                    bubbleSort ( products );
+                    unsorted = true;
                 }
             }
         }
