96fdbcea8b6582fa3c123f403c304b151c0e7619
molodoysever <dpnofficial1@gmail.com>
2022-11-18 18:46:36 +0200

    hw21

---
+++ src/main/java/ua/lyashko/lesson21/Car.java
@@ -0,0 +1,22 @@
+package ua.lyashko.lesson21;
+
+public class Car {
+    private String name;
+    private String fuel;
+
+    public String getName () {
+        return name;
+    }
+
+    public String getFuel () {
+        return fuel;
+    }
+
+    public void setName ( String name ) {
+        this.name = name;
+    }
+
+    public void setFuel ( String fuel ) {
+        this.fuel = fuel;
+    }
+}
---
+++ src/main/java/ua/lyashko/lesson21/Validate.java
@@ -0,0 +1,12 @@
+package ua.lyashko.lesson21;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+
+public @interface Validate {
+}
