1ab230f5850a5ea20a7e5c695eab0b9c48c62d20
molodoysever <dpnofficial1@gmail.com>
2022-11-19 02:02:50 +0200

    hw22

---
+++ src/main/resources/sql/hw22/task3.sql
@@ -0,0 +1,29 @@
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (1 ,'Andrew', 'Jackson', '2000-10-08', 'USA', 'male');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (2 ,'Svetlana', 'Ivanova', '1999-10-08', 'Ukraine', 'female');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (3 ,'Giovanni', 'Pasta', '1994-10-08', 'Italy', 'male');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (4 ,'Buenos', 'Paella', '2005-10-08', 'Spain', 'male');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (5 ,'Anton', 'Petrov', '1954-10-08', 'Ukraine', 'male');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (6 ,'Andrew', 'Stan', '1933-10-08', 'USA', 'male');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (7 ,'Stepan', 'Bandera', '1909-01-01', 'Ukraine', 'male');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (8 ,'Erling', 'Haaland', '2001-10-08', 'Norway', 'male');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (9 ,'Svino', 'Sobaka', '1488-05-10', 'Russia', 'female');
+
+INSERT INTO Customer (login, firstName, lastName, dateOfBirth, country, sex)
+VALUES (10 ,'Leon', 'Kooks', '1998-10-08', 'Finland', 'male');
