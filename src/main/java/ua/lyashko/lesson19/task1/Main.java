7b54263f2d1c20c69a59d231b429c47f8e1cd9dc
molodoysever <dpnofficial1@gmail.com>
2022-12-01 16:04:14 +0200

    hw19 reworked reader

--- src/main/java/ua/lyashko/lesson19/task1/Main.java
+++ src/main/java/ua/lyashko/lesson19/task1/Main.java
@@ -1,33 +1,34 @@
 package ua.lyashko.lesson19.task1;
 
 import java.io.*;
-import java.util.Objects;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main ( String[] args ) throws IOException {
         File file = new File ( "src/main/resources/test.csv" );
         Scanner scanner = new Scanner ( System.in );
-        writer ( file , scanner );
+        writer ( file , reader ( scanner ) );
     }
 
-    private static String reader ( Scanner scanner ) {
+    private static List<String> reader ( Scanner scanner ) {
         String line = null;
-        line = scanner.nextLine ( );
-        return line;
+        List<String> strings = new ArrayList<> ( );
+        while ( !Objects.equals ( line , "end" ) ) {
+            line = scanner.nextLine ( );
+            if (line.contentEquals ( "next" )) {
+                strings.add ( "\n" );
+            } else if (line.contentEquals ( "end" )) {
+
+            } else {
+                strings.add ( line );
+            }
+        }
+        return strings;
     }
 
-    private static void writer ( File file , Scanner scanner ) {
+    private static void writer ( File file , List<String> strings ) {
         try (FileWriter writer = new FileWriter ( file )) {
-            String line = null;
-            while ( !Objects.equals ( line , "end" ) ) {
-                line = reader ( scanner );
-                if (line.contentEquals ( "next" )) {
-                    writer.write ( "\n" );
-                } else {
-                    writer.write ( line );
-                }
-            }
+            writer.write ( String.valueOf ( strings ) );
         } catch ( IOException e ) {
             e.printStackTrace ( );
         }
