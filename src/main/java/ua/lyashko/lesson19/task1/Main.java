a7ef776ce6fb3f3604f8c47f0849509843dc8db7
molodoysever <dpnofficial1@gmail.com>
2022-12-01 15:45:14 +0200

    hw19 reworked methods

--- src/main/java/ua/lyashko/lesson19/task1/Main.java
+++ src/main/java/ua/lyashko/lesson19/task1/Main.java
@@ -1,26 +1,35 @@
 package ua.lyashko.lesson19.task1;
 
 import java.io.*;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class Main {
     public static void main ( String[] args ) throws IOException {
         File file = new File ( "src/main/resources/test.csv" );
         Scanner scanner = new Scanner ( System.in );
+        writer ( file , scanner );
+    }
 
+    private static String reader ( Scanner scanner ) {
+        String line = null;
+        line = scanner.nextLine ( );
+        return line;
+    }
+
+    private static void writer ( File file , Scanner scanner ) {
         try (FileWriter writer = new FileWriter ( file )) {
-            while ( scanner.hasNext ( ) ) {
-                String line = scanner.nextLine ( );
-
-                if (line.contentEquals ( "end" )) {
-                    writer.flush ( );
-                    scanner.close ( );
-                } else if (line.contentEquals ( "next" )) {
+            String line = null;
+            while ( !Objects.equals ( line , "end" ) ) {
+                line = reader ( scanner );
+                if (line.contentEquals ( "next" )) {
                     writer.write ( "\n" );
                 } else {
                     writer.write ( line );
                 }
             }
+        } catch ( IOException e ) {
+            e.printStackTrace ( );
         }
     }
 }
