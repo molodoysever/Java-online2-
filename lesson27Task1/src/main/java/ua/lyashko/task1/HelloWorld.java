725f2a85fade8da9496a0bd4f2c71b2c306ff932
molodoysever <dpnofficial1@gmail.com>
2022-12-13 00:00:44 +0200

    hw 28

--- lesson27Task1/src/main/java/ua/lyashko/task1/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package ua.lyashko.task1;
-
-public class HelloWorld {
-    public static void main ( String[] args ) {
-        System.out.println ("Hello Docker!" );
-    }
-}
--- lesson28/src/main/java/ua/lyashko/Main.java
+++ lesson28/src/main/java/ua/lyashko/Main.java
@@ -1,21 +1,26 @@
-package ua.lyashko.task2;
+package ua.lyashko;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class DatabaseConnection {
+public class Main {
     public static void main ( String[] args ) {
         Connection connection = null;
         String user = "root";
         String password = "Password123!";
         String url = "jdbc:mariadb://localhost:3306/";
+
         try {
-            connection = DriverManager.getConnection ( url, user, password);
-            Statement statement = connection.createStatement ( );
-            String str = "CREATE database Hello;";
-            statement.executeUpdate ( str );
+            connection = DriverManager.getConnection ( url, user, password );
+            if (connection.isClosed ()) {
+                System.out.println ("connection closed" );
+            } else {
+                Statement statement = connection.createStatement ();
+                String str = "CREATE database Hello;";
+                statement.executeUpdate ( str );
+            }
         } catch ( SQLException e ) {
             e.printStackTrace ( );
         }
